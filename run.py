ecb62498f4fdbcd8cc96faa0a4b6c3da2acce2f1
Anshul Rajput <anshulrajput@Anshuls-MacBook-Air.local>
2021-10-02 01:18:35 +0530

    modify run.py layout.html and lecture_hall.html delete main.css add error.html and time_slot.html

--- run.py
+++ run.py
@@ -1,17 +1,101 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+time_slots = ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"]
+
+database = {
+    'Lecture Hall 1': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 2': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 3': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 4': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 5': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 6': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 7': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+                       "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+                       "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 8': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 9': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 10': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 11': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 12': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 13': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 14': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 15': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"],
+    'Lecture Hall 16': ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm",
+              "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm", "2:00 pm - 3:00 pm", "3:00 pm - 4:00 pm",
+              "4:00 pm - 5:00 pm", "5:00 pm - 6:00 pm", "6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"]
+}
+
 
 @app.route("/")
-def hello_world():
-    return render_template('home.html', title='Home')
+def home():
+    lecture_halls = []
+    for lecture_hall in database:
+        if len(database[lecture_hall]) == len(time_slots):
+            is_available = False
+        else:
+            is_available = True
+        lecture_halls.append([lecture_hall, is_available])
+    any_lecture_hall_available = any(lecture_hall[1] for lecture_hall in lecture_halls)
+    return render_template('lecture_hall.html', title='Home', lecture_halls=lecture_halls,
+                           any_lecture_hall_available=any_lecture_hall_available)
 
 
-@app.route("/lecture_hall/<int:number>")
-def lecture_hall(number):
-    return render_template('lecture_hall.html', title=f'Lecture Hall {number}', number=number)
+@app.route("/time_slot_page", methods=['GET', 'POST'])
+def time_slot_page():
+    selected_lecture_hall = request.form.get('lecture-hall')
+    if selected_lecture_hall:
+        booked_time_slots = []
+        for time_slot in database[selected_lecture_hall]:
+            booked_time_slots.append(time_slot)
+        return render_template('time_slot.html', title='Time Slots', time_slots=time_slots,
+                               booked_time_slots=booked_time_slots, selected_lecture_hall=selected_lecture_hall)
+    error = 'You must select the lecture hall'
+    return render_template('error.html', error=error)
+
+
+@app.route("/booking", methods=['GET', 'POST'])
+def booking_page():
+    selected_time_slot = request.form.get('time-slot')
+    current_lecture_hall = request.form.get('current-lecture-hall')
+    if selected_time_slot and current_lecture_hall:
+        database[current_lecture_hall].append(selected_time_slot)
+        return redirect('/')
+    error = "You must select the time slot"
+    return render_template('error.html', error=error)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
